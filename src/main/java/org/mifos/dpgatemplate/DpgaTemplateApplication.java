2b5e9ae1ae123423d0ac342efcbb9fd77a4c7138
Anover000 <ankit@fynarfin.io>
2023-09-21 14:16:16 +0530

    initial commit

---
+++ src/main/java/org/mifos/dpgatemplate/DpgaTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.mifos.dpgatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpgaTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpgaTemplateApplication.class, args);
+    }
+
+}
